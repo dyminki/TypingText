c5ac7f95b7bda11f438054d9c6887e36447780ac
Admin <admin@MacBook-Pro-Admin.local>
2018-08-01 10:15:50 +0200

    tests fixed

--- js/__test__/TypingText.test.js
+++ js/__test__/TypingText.test.js
@@ -15,11 +15,11 @@ describe('<TextTyper />', () => {
   
     it('have correct props', () => {
       const wrapper = shallow(<TypingText text={['1 text', '2 text', '3 text', '4 text', '5 text']} />);
-      expect(wrapper.instance().props.text[0]).to.equal('1 text');
-      expect(wrapper.instance().props.text[1]).to.equal('2 text');
-      expect(wrapper.instance().props.text[2]).to.equal('3 text');
-      expect(wrapper.instance().props.text[3]).to.equal('4 text');
-      expect(wrapper.instance().props.text[3]).to.equal('5 text');
+      expect(wrapper.instance().props.text[0]).toBe('1 text');
+      expect(wrapper.instance().props.text[1]).toBe('2 text');
+      expect(wrapper.instance().props.text[2]).toBe('3 text');
+      expect(wrapper.instance().props.text[3]).toBe('4 text');
+      expect(wrapper.instance().props.text[4]).toBe('5 text');
     });
     
   });
