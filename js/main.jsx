8fc8ef0ddbf0f31969be016ee1b8cfe061b45f77
dyminki <barbara.dyminkiewicz@gmail.com>
2019-01-29 21:38:32 +0100

    code clean up

--- js/main.jsx
+++ js/main.jsx
@@ -1,13 +1,10 @@
 import "babel-polyfill";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components/App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App.jsx";
 
 require("../scss/main.scss");
 
-document.addEventListener('DOMContentLoaded', function(){
-    ReactDOM.render(
-        <App />,
-        document.getElementById('app')
-    );
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(<App />, document.getElementById("app"));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,104 +1,98 @@
 var path = require("path");
-var Html = require('html-webpack-plugin');
+var Html = require("html-webpack-plugin");
 var MiniCSS = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = function(env) {
-    const isDev = env && env.dev ? true : false;
-    console.log(isDev, 'isDev');
+  const isDev = env && env.dev ? true : false;
+  console.log(isDev, "isDev");
 
-    const config = {
-        entry: "./js/main.jsx",
-        output: {
-            filename: "out.js",
-            path: path.resolve(__dirname, "docs")
+  const config = {
+    entry: "./js/main.jsx",
+    output: {
+      filename: "out.js",
+      path: path.resolve(__dirname, "docs")
+    },
+    mode: isDev ? "development" : "production",
+    module: {
+      noParse: /(mapbox-gl)\.js$/,
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2015", "stage-2", "react"]
+            }
+          }
         },
-        mode: isDev ? 'development' : 'production',
-        module: {
-            noParse: /(mapbox-gl)\.js$/,
-            rules: [
-                {
-                    test: /\.jsx$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', "stage-2", "react"]
-                        }
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        isDev ? 'style-loader' : MiniCSS.loader,
-                        'css-loader'
-                    ]
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        isDev ? 'style-loader' : MiniCSS.loader,
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: () => [
-                                    new require('autoprefixer')({
-                                        browsers: [
-                                            'ie 11' // tu definiujemy wsparcie dla przegladarek w css
-                                        ]
-                                    })
-                                ]
-                            }
-                        },
-                        'sass-loader'
-                    ]
-                },
-                {
-                    test: /\.(jpg|jpeg|gif|png|csv)$/,
-                    use: {
-                        loader: 'file-loader?outputPath=images/',
-                        options: {
-                            name: '[name].[ext]',
-                            useRelativePath: true
-                        }
-                    }
-                },
-                {
-                    test: /\.(eot|ttf|woff|woff2)$/,
-                    use: {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            publicPath: 'fonts',
-                            outputPath: 'fonts'
-                        }
-                    }
-                }
-            ]
+        {
+          test: /\.css$/,
+          use: [isDev ? "style-loader" : MiniCSS.loader, "css-loader"]
         },
+        {
+          test: /\.scss$/,
+          use: [
+            isDev ? "style-loader" : MiniCSS.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                plugins: () => [
+                  new require("autoprefixer")({
+                    browsers: [
+                      "ie 11" // tu definiujemy wsparcie dla przegladarek w css
+                    ]
+                  })
+                ]
+              }
+            },
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.(jpg|jpeg|gif|png|csv)$/,
+          use: {
+            loader: "file-loader?outputPath=images/",
+            options: {
+              name: "[name].[ext]",
+              useRelativePath: true
+            }
+          }
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              publicPath: "fonts",
+              outputPath: "fonts"
+            }
+          }
+        }
+      ]
+    },
 
+    plugins: [
+      new Html({
+        filename: "index.html",
+        template: "./index.html"
+      }),
+      new MiniCSS({
+        filename: "app.css" // definiujemy adres pliku css
+      }),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            warnings: true,
+            comparisons: false
+          }
+        }
+      })
+    ]
+  };
 
-        plugins: [
-            new Html({
-                filename: 'index.html',
-                template: './index.html'
-            }),
-            new MiniCSS({
-                filename: "app.css", // definiujemy adres pliku css
-            }),
-            new UglifyJsPlugin({
-                uglifyOptions:{
-                    compress: {
-                        warnings: true,
-                        comparisons: false
-                    }
-                }
-            })
-
-        ]
-    }
-
-    return config;
-}
+  return config;
+};
